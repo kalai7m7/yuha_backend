3736fb1733dad0e456b8e9d4bcf9ea116adcc1f2
Kalaivani <kalai7mdhoni@gmail.com>
2026-08-17 12:40:46 +0530

    supabase auth added

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { productRouter } from './modules/products/product.routes';
 import { catalogRouter } from './modules/catalog/catalog.routes';
+import { authRouter } from './modules/auth/auth.routes';
 
 export const apiRouter = Router();
 
+apiRouter.use('/auth', authRouter);
 apiRouter.use('/products', productRouter);
 apiRouter.use('/catalog', catalogRouter);
